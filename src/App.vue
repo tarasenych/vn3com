b56b1720fbc269d50307747966e1c39b54af7af9
Roman Tarasenko <r.tarasenko@etpgpb.ru>
2025-12-28 17:04:28 +0300

    Убрал лишнее, добавил Telegram SDK

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import WebApp from '@twa-dev/sdk'
+import { onMounted, ref } from 'vue'
+
+const user = ref(WebApp.initDataUnsafe.user)
+
+onMounted(() => user.value = WebApp.initDataUnsafe.user)
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  Привет,
+  <pre>
+    {{ user }}
+  </pre>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
-import './style.css'
+import WebApp from '@twa-dev/sdk'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const init = async () => {
+  // 1. Сообщаем Telegram, что мы готовы. 
+  // Это убирает загрузочный экран Telegram.
+  WebApp.ready()
+
+  // 2. Опционально: можно добавить микро-задержку для стабилизации стилей темы
+  // или дождаться, пока Telegram передаст расширенные параметры темы
+  if (WebApp.colorScheme) {
+     console.log('Тема определена:', WebApp.colorScheme)
+  }
+
+  // 3. Монтируем приложение
+  const app = createApp(App)
+  app.mount('#app')
+};
+
+init()
